633f0c0feebb4a444a7292547b78b1d4db456201
Aquerr <nerdianin@gmail.com>
2021-04-23 16:08:16 +0200

    Initial commit

---
+++ src/main/java/pl/bartlomiejstepien/basemobsiege/BaseMobSiege.java
@@ -0,0 +1,20 @@
+package pl.bartlomiejstepien.basemobsiege;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BaseMobSiege extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
